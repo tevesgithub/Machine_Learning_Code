d726c0a759a3981c00e5d5cb1d34c3f5d6d6b5e1
tevesgithub <vjteves@gmail.com>
2024-10-07 15:33:39 -0600

    update

--- linear_regression.py
+++ linear_regression.py
@@ -1,16 +1,16 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression 
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
 np.random.seed(34)
-
+   
 #Generate synthetic data
-X = 2 * np.random.rand(100, 1) #Generate 100 random numbers between 0 & 2
-y = 4 + 4 * X + np.random.randn(100,1) #Linear relationship with noise
+X = 2 * np.random.rand(100, 1)  # Generate 100 random numbers between 0 & 2
+y = 4 + 4 * X + np.random.randn(100, 1) #Linear relationship with noise
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state =42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
--- training_ML_model.py
+++ training_ML_model.py
@@ -1,24 +1,39 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import joblib
 
-
-
-music_data = pd.read_csv("music.csv")
+music_data = pd.read_csv('music.csv')
 
 X = music_data.drop(columns=['genre']) #dropping the target values
 
-y = music_data ['genre'] #target or output values
+y = music_data['genre']  #target or output values
 
-model = DecisionTreeClassifier()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-model.fit(X_train, y_train)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-predictions = model.predict(X_test)
+#model.fit(X_train, y_train)
 
-score = accuracy_score(y_test, predictions)
+#joblib.dump(model, 'music-recommender.joblib')
 
-print (score)
+
+model = joblib.load('music-recommender.joblib')
+
+
+predictions = model.predict([[21, 1]])
+
+print(predictions)
+
+#score = accuracy_score(y_test, predictions)
+
+#print(score)
+
+tree.export_graphviz(model, out_file='music-recommender.dot', feature_names=['age', 'gender'],
+                     class_names=sorted(y.unique()),
+                     label='all',
+                     rounded=True,
+                     filled=True,
+                     )
